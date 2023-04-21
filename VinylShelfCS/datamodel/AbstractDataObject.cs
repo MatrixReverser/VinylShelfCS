09a07534042d2ccfdaf57dc62d83ba241becb79b
Andreas Altwein <kontakt@andreas-altwein.de>
2023-04-21 21:00:57 +0200

    Initial commit

---
+++ VinylShelfCS/datamodel/AbstractDataObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylShelfCS.datamodel
+{
+    /*
+     * Base class for all data objects of the application
+     */
+    internal abstract class AbstractDataObject
+    {
+        // Class variables
+        private static int id_generator = 0;
+
+        // Member variables
+        private int _id;
+
+        // Properties
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                if (id_generator <= _id)
+                {
+                    id_generator++;
+                }
+            }
+        }
+
+        protected AbstractDataObject()
+        {
+            Id = id_generator;
+        }
+    }
+}
---
+++ VinylShelfCS/datamodel/GenreDO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylShelfCS.datamodel
+{
+    internal class GenreDO : AbstractDataObject
+    {
+        // Constants
+        private const string DEFAULT_NAME = "<Genre>";
+
+        // Properties
+        public string Name { get; set; }
+
+        public GenreDO()
+        {
+            Name = DEFAULT_NAME;
+        }
+    }
+}
